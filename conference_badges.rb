68a23c92879d5aacfb8f12bdf9a6d7b3fa9c7732
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-05-29 19:25:12 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def badge_maker(name)
     return "Hello, my name is #{name}."
 end  
@@ -10,17 +12,25 @@ end
 
 def assign_rooms(speakers)  
     counter = 1 
-    speakers.map do |speaker|
-        "Hello, #{speaker}! You'll be assigned to room #{counter}!"
-
+    speakers.each_with_index.map do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+    end 
 end 
+
+def printer(attend)
+    first = batch_badge_creator(attend) 
+    second = assign_rooms(attend) 
+
+    first.each do |name|
+    puts name 
+    end 
+
+    second.each do |name|
+        puts name 
+    end 
+
+   
 end 
-
-
-
-
-
-
 
 
 
