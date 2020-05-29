a62df830cf543b19126d16187df659606ebc491a
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-05-29 18:57:19 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end  
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end 
+end 
+
+def assign_rooms(speakers)  
+    counter = 1 
+    speakers.map do |speaker|
+        "Hello, #{speaker}! You'll be assigned to room #{counter}!"
+
+end 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
